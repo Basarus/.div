6739fe7f7b5237325d5cf340322263044c1e07ef
bazaq <basarus51@gmail.com>
2023-06-28 17:56:41 +0300

    upd

--- src/utls/index.ts
+++ src/utls/index.ts
@@ -1,7 +1,13 @@
 import { Role } from "src/roles/roles.model";
 import * as CONFIG from './config';
+import { User } from "src/users/users.model";
 
 export async function createTestDatabase() {
     let roles = await Role.findAll();
-    if (roles.length <= 0) CONFIG.ROLES.forEach(async e => Role.create({name: e}))
+    if (roles.length <= 0) {
+        CONFIG.ROLES.forEach(async e => Role.create({name: e}))
+        const user = await User.create({email: 'admin@admin.ru', name: 'admin', password: 'admin'})
+        const role = await this.roleService.getRoleById('Администратор');
+        await user.$set('roles', [role.id]);
+    }
 }
